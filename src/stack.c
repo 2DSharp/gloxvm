cee639c4cd0f13daf7667e67144714ead21f98eb
2dsharp <30053071+2Dsharp@users.noreply.github.com>
2019-04-22 00:38:19 +0530

    Creation of a new stack returns a stack ptr instead

--- src/stack.c
+++ src/stack.c
@@ -1,19 +1,31 @@
 #include "stack.h"
+#include "stdio.h"
 
-Stack stack_new(int size)
+Stack * stack_new(int size)
 {
-  Stack stack;
-  stack.top = -1;
-  stack.size = size;
-  stack.contents = malloc(sizeof(stack_obj_t) * size);
+  Stack * stack = malloc(sizeof(Stack));
+  stack->top = -1;
+  stack->size = size;
+  stack->contents = malloc(sizeof(stack_obj_t) * size);
 
   return stack;
 }
 
 int stack_push(Stack * stack, int val)
 {
-  stack->contents[stack->top++] = val;
+  stack->contents[++stack->top] = val;
   return stack->top;
 }
+stack_obj_t stack_pop(Stack * stack)
+{
+  return stack->contents[stack->top--];
+}
 
-
+void stack_debug_print(Stack * stack)
+{
+  printf("[ ");
+  for ( int i = stack->top; i > -1; i--) {
+    printf("%d ", stack->contents[i]);
+  }
+  printf("]");
+}
