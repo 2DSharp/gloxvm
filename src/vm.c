28ab6f117116e823a3e42fa6ef5c39c376c057cb
2dsharp <30053071+2Dsharp@users.noreply.github.com>
2019-04-22 00:38:52 +0530

    Fix bugs and add more debugging info

--- src/vm.c
+++ src/vm.c
@@ -2,12 +2,8 @@
 
 VM * vm_init(uint32_t stack_size)
 {
-
-  Stack stack;
-  stack = stack_new(stack_size);
-
   VM * vm = (VM*) malloc(sizeof(VM));
-  vm->stack = &stack;
+  vm->stack = stack_new(stack_size);
   vm->frame_ptr = -1;
   vm->instr_ptr = 0;
 
@@ -16,15 +12,18 @@ VM * vm_init(uint32_t stack_size)
 
 void vm_run(VM * vm, Code * code)
 {
-  short opcode = NOP;
+  short opcode;
   opcode_runner runners[128];
   
   opcode_runner_init(runners);
+  opcode = code_fetch(code, vm->instr_ptr);
   
   while (opcode != HALT) {
-    printf("Opcode: %d\t IP: %d\n", opcode, vm->instr_ptr);
-    vm->instr_ptr = runners[opcode](vm->stack, vm->instr_ptr);
+    printf("Opcode: %d\t IP: %d\t", opcode, vm->instr_ptr);
+    vm->instr_ptr = runners[opcode](code, vm->stack, vm->instr_ptr);
     opcode = code_fetch(code, vm->instr_ptr);
+    stack_debug_print(vm->stack);
+    printf("\tTop: %d\n", vm->stack->top);
   }
     
 }
