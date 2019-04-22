e7f4fc5f2eceaad5ac4b5de12cac05cc9aae7502
2dsharp <30053071+2Dsharp@users.noreply.github.com>
2019-04-22 19:11:37 +0530

    Statically init stack contents and implement load/store

--- src/stack.c
+++ src/stack.c
@@ -3,11 +3,13 @@
 
 Stack * stack_new(int size)
 {
-  Stack * stack = malloc(sizeof(Stack *));
+  Stack * stack = malloc(sizeof(Stack));
   stack->top = -1;
+  stack->frame_ptr = -1;
   stack->size = size;
-  stack->contents = malloc(sizeof(stack_obj_t) * size);
   
+  int stack_arr[size];
+  stack->contents = stack_arr;
   return stack;
 }
 
@@ -32,6 +34,24 @@ void stack_debug_print(Stack * stack)
 
 void stack_flush(Stack * stack)
 {
-  free(stack->contents);
+  //free(stack->contents);
   free(stack);
+}
+
+void stack_load(Stack * stack, short offset)
+{
+  int addr = stack->frame_ptr - offset;
+
+  stack_push(stack, stack->contents[addr]);
+}
+
+void stack_store(Stack * stack, short offset)
+{
+  int addr = stack->frame_ptr - offset;
+
+  //printf("Addr: %d", addr);
+  int v = stack_pop(stack);
+
+  stack->contents[addr] = v;
+  //printf("Content: %d", stack->contents[addr]);
 }
