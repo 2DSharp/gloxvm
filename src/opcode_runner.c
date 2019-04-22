bbc534ca83b0ad53a43643204f219a599c9a3177
2dsharp <30053071+2Dsharp@users.noreply.github.com>
2019-04-22 19:56:27 +0530

    Update ip from here, since jmp would return a new ip

--- src/opcode_runner.c
+++ src/opcode_runner.c
@@ -22,7 +22,7 @@ int exec_isub(Code * code, Stack * stack, int ip)
   
   stack_push(stack, a - b);
   
-  return ip;
+  return ++ip;
 }
 
 int exec_imul(Code * code, Stack * stack, int ip)
@@ -32,7 +32,7 @@ int exec_imul(Code * code, Stack * stack, int ip)
 
   stack_push(stack, a * b);
   
-  return ip;
+  return ++ip;
 }
 
 int exec_idiv(Code * code, Stack * stack, int ip)
@@ -42,33 +42,38 @@ int exec_idiv(Code * code, Stack * stack, int ip)
   
   stack_push(stack, a / b);
   
-  return ip;
+  return ++ip;
 }
 
 int exec_iconst(Code * code, Stack * stack, int ip)
 {
   short v = code_fetch(code, ++ip);
   stack_push(stack, v);
-  return ip;
+  return ++ip;
 }
 
 int exec_print(Code * code, Stack * stack, int ip)
 {
   short v = stack_pop(stack);
   printf("%d", v);
-  return ip;
+  return ++ip;
 }
 
 int exec_load(Code * code, Stack * stack, int ip)
 {
   stack_load(stack, code_fetch(code, ++ip));
-  return ip;
+  return ++ip;
 }
 
 int exec_store(Code * code, Stack * stack, int ip)
 {
   stack_store(stack, code_fetch(code, ++ip));
-  return ip;
+  return ++ip;
+}
+
+int exec_jmp(Code * code, Stack * stack, int ip)
+{
+  return code_fetch(code, ++ip);
 }
 
 void opcode_runner_init(opcode_runner * ops)
@@ -82,4 +87,5 @@ void opcode_runner_init(opcode_runner * ops)
   ops[PRINT] = exec_print;
   ops[LOAD] = exec_load;
   ops[STORE] = exec_store;
+  ops[JMP] = exec_jmp;
 }
