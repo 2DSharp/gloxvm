7911b891676be8a3bd747f6b4645da3721a58521
2dsharp <30053071+2Dsharp@users.noreply.github.com>
2019-04-22 22:13:54 +0530

    Add conditional loop test

--- src/glox.c
+++ src/glox.c
@@ -14,7 +14,7 @@ int main(int argc, char **argv )
     stack_size = atoi(argv[1]);
   }
   vm = vm_init(stack_size);
-  short instrs[] = { ICONST, 0, ICONST, 1, IADD, PRINTLN, JMP, 2, HALT };
+  short instrs[] = { ICONST, 0, STORE, 1, LOAD, 1, ICONST, 1, IADD, PRINTLN, STORE, 1, LOAD, 1, ICONST, 50, ILT, JMPT, 4, HALT };
   Code * code = code_init(instrs, 2);
 
   printf("IP: %d\n", vm->instr_ptr);
