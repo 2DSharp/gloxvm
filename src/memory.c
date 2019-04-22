474be0e532aea872b5ebd81157c43b72cfc8e6c1
2dsharp <30053071+2Dsharp@users.noreply.github.com>
2019-04-23 02:37:41 +0530

    Fix invalid accesses in mem allocation
    allocate the size of Memory, not (Memory *)
    Fill locals array with 0

--- src/memory.c
+++ src/memory.c
@@ -2,12 +2,14 @@
 
 Memory * mem_init(unsigned int local_sz, unsigned int global_sz)
 {
-  Memory * mem = malloc(sizeof(Memory *));
+  Memory * mem = malloc(sizeof(Memory));
   mem->global_sz = local_sz;
   mem->local_sz = global_sz;
 
   short locals[local_sz];
+  for (int i = 0; i < local_sz; i++) {
+    locals[i] = 0;
+  };
   mem->locals = locals;
-
   return mem;
 }
