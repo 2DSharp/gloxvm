1977afa6df3313dfbd5a64156b7d1972e7970055
2dsharp <30053071+2Dsharp@users.noreply.github.com>
2019-04-24 00:05:58 +0530

    Remove unimplemented functions

--- src/vm.h
+++ src/vm.h
@@ -16,8 +16,6 @@
 VM * vm_init(size_t stack_size, Memory * mem);
 void vm_run(VM * vm, Code * code, int debug);
 void vm_close(VM * vm);
-void vm_fetch();
-Opcode vm_decode(short opcode);
-
+void vm_exec(VM * vm, const Code * code_mem, const Opcode * opcode);
 
 #endif /* vm.h */
