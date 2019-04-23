40263da1c23a5161873dd0c54855bc4540bab661
2dsharp <30053071+2Dsharp@users.noreply.github.com>
2019-04-23 22:45:40 +0530

    Record vm state

--- src/vm_struct.h
+++ src/vm_struct.h
@@ -4,11 +4,15 @@
 #include "stack.h"
 #include "memory.h"
 
+typedef enum state_t
+  { ST_RUNNING, ST_HALTED, ST_INVALID } State;
+
 typedef struct vm_t
 {
   int instr_ptr;
   Stack * stack;
   Memory * memory;
+  State state;
 } VM;
 
 #endif
